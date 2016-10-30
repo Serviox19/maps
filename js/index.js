cdb72edb3b60d893f64e8571a023712641582fb7
Servio Mora <servio.mora19@gmail.com>
2016-10-30 12:36:13 -0400

    added click event to get current location

--- js/index.js
+++ js/index.js
@@ -1,33 +1,39 @@
 $(document).ready(function () {
 
+  var lat;
+  var lon;
+
   var map = new GMaps({
     div: '#map',
-    zoom: 16,
-    lat: -12.043333,
-    lng: -77.028333,
+    zoom: 12,
+    lat: 40.7562,
+    lng: -73.9868,
     click: function(e) {
       alert('click');
     },
     dragend: function(e) {
-      alert('dragend');
+      console.log('dragged');
     }
   });
 
-  GMaps.geolocate({
-    success: function(position) {
-      map.setCenter(position.coords.latitude, position.coords.longitude);
-    },
-    error: function(error) {
-      alert('Geolocation failed: '+error.message);
-    },
-    not_supported: function() {
-      alert("Your browser does not support geolocation");
-    },
-    always: function() {
-      console.log("Done!");
-    }
+  $('#getLocation').click(function() {
+
+    GMaps.geolocate({
+      success: function(position) {
+        map.setCenter(position.coords.latitude, position.coords.longitude);
+        var lat = position.coords.latitude;
+        var lon = position.coords.longitude;
+      },
+      error: function(error) {
+        alert('Geolocation failed: '+error.message);
+      },
+      not_supported: function() {
+        alert("Your browser does not support geolocation");
+      },
+      always: function() {
+        console.log("Done!");
+      }
+    });
+
   });
-
-
-
 });
